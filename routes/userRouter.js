867a563c8629bb2cdcfca9fd09089216801bba41
JoelMaciel <soldado25461@gmail.com>
2021-02-17 02:24:13 -0300

    controllers

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,15 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController')
 
 
-router.post('/register', (req, res) =>{
-    console.log('register')
-    res.send('Resgister')
- })
-router.post('/login', (req, res) => { 
-    console.log('login')
-    res.send('login')
-})
+router.post('/register', userController.register)
+router.post('/login', userController.login)
 
 
 module.exports = router;
