42001d802b88deef7718cba07fe4ea16fca31e99
JoelMaciel <soldado25461@gmail.com>
2021-02-17 22:49:09 -0300

    connection  mongo.atlas

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ require('dotenv').config();;
 const express = require('express');
 const app = express();
 const userRouter = require('./routes/userRouter')
+const mongoose = require('mongoose')
 
 
-app.use('/user', userRouter);
+
+mongoose.connect(process.env.MONGO_CONNECTION_URL,
+     {
+    useNewUrlParser:true,
+     useUnifiedTopology: true
+    },
+    (error)=> {
+        if (error)
+            console.log(error)
+        else
+        console.log("Mongo Connected")
+        
+    })
+
+
+app.use('/user',express.json(), userRouter);
 
 
 app.listen(process.env.PORT, () => {console.log("Server Running")})
